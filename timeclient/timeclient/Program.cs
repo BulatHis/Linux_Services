b3335ae7a212e1da5a7c552f9c4129771c67da3f
Bulat <bulatxisamee@gmail.com>
2023-10-04 21:18:45 +0300

    1st

---
+++ timeclient/timeclient/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Это правильный IP адрес  - 127.0.0.1?");
+        Console.WriteLine("Если да - нажмите Enter, если нет, то напишите - 'no'");
+        string serverIp = "127.0.0.1";
+        string answer = Console.ReadLine();
+        
+        if (answer == "no")
+        {
+            Console.WriteLine("Введите IP адрес сервера");
+            serverIp = Console.ReadLine();
+        }
+        ConnectToServer(serverIp).Wait();
+    }
+
+    private static async Task ConnectToServer(string serverIp)
+    {
+        TcpClient client = new TcpClient();
+
+        try
+        {
+            await client.ConnectAsync(serverIp, 1303);
+
+            using (NetworkStream stream = client.GetStream())
+            {
+                byte[] buffer = new byte[1024];
+                int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+                string response = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+                Console.WriteLine("Получен ответ от сервера: " + response);
+                await Task.Delay(4000);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Ошибка подключения к серверу: " + ex.Message);
+        }
+        finally
+        {
+            client.Close();
+        }
+    }
+}
---
+++ timeserver/timeserver/Program.cs
@@ -0,0 +1,46 @@
+﻿
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        await StartServerAsync();
+    }
+
+    private static async Task StartServerAsync()
+    {
+        TcpListener listener = new TcpListener(IPAddress.Any, 1303);
+        listener.Start();
+        Console.WriteLine("timeserver started. Listening on port 1303...");
+
+        while (true)
+        {
+            TcpClient client = await listener.AcceptTcpClientAsync();
+            Console.WriteLine("Client connected.");
+
+            try
+            {
+                using (NetworkStream stream = client.GetStream())
+                {
+                    string response = DateTime.Now.ToString("dd.MM.yyyy HH:mm");
+                    byte[] data = Encoding.UTF8.GetBytes(response);
+                    await stream.WriteAsync(data, 0, data.Length);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                client.Close();
+                Console.WriteLine("Connection closed.");
+            }
+        }
+    }
+}
